a0c39533216794305a2de4686939ecc73fb1361f
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-09-15 18:09:00 +0100

    fix bug with search function

--- src/components/FilmFilter.vue
+++ src/components/FilmFilter.vue
@@ -1,6 +1,10 @@
 <template lang="html">
   <form v-on:submit.prevent>
-      <input type="text" v-model="search" placeholder="search for film..." v-on:keyup="searchForFilm">
+      <input type="text" v-model="search" placeholder="Search for film..." v-on:keyup="searchForFilm">
+      <!-- <select v-on:change="handleSelect" v-model="selectedFilm">
+          <option disabled value="">Select a film</option>
+          <option v-for="film in films" :value="film">{{film.title}}</option>
+      </select> -->
   </form>
 </template>
 
@@ -12,18 +16,22 @@ export default {
     data() {
         return {
             "search": "",
-            "selectedFilm": {},
+            "selectedFilm": {}
         }
     },
     props: ["films"],
     methods: {
         searchForFilm(){
             let foundFilm = this.films.find((film) => {
-                return film.title.toLowerCase().indexOf(this.search.toLowerCase() > -1)
+                return film.title.toLowerCase().indexOf(this.search.toLowerCase()) > -1
             })
             this.selectedFilm = foundFilm   
             eventBus.$emit('film-selected', this.selectedFilm)
         }
+        // handleSelect(){
+        //     this.search = ""
+        //     eventBus.$emit('film-selected', this.selectedFilm)
+        // }
     }
 }
 </script>
